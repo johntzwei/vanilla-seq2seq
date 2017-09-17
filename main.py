9fe97eaca2c69cbee99a733487f278be7da1132b
Johnny Wei <jwei@gypsum.cm.cluster>
2017-09-17 17:00:26 -0400

    fixed architecture with teacher forcing

--- main.py
+++ main.py
@@ -6,13 +6,16 @@ import time
 import random
 random.seed(0)
 
+import _gdynet as dy
+dy_params = dy.DynetParams()
+dy_params.set_random_seed(random.randint(0, 1000))
+dy_params.set_requested_gpus(1)
+dy_params.set_weight_decay(0.001)
+dy_params.init()
+
 import numpy as np
-import dynet_config
-dynet_config.set_gpu()
-dynet_config.set(mem=4096, \
-        random_seed=random.randint(1, 100),
-    )
-import dynet as dy
+
+from model import Seq2SeqAttention
 
 def ptb(section='test.txt', directory='ptb/', padding='<EOS>', column=0):
     with open(os.path.join(directory, section), 'rt') as fh:
@@ -37,7 +40,7 @@ def text_to_sequence(texts, vocab):
 
 def sort_by_len(X, y):
     data = list(zip(X, y))
-    data.sort(key=lambda x: len(x[1]))
+    data.sort(key=lambda x: -len(x[1]))
     return [ i[0] for i in data ], [ i[1] for i in data ]
 
 def batch(X, batch_size, mask=0.):
@@ -46,142 +49,23 @@ def batch(X, batch_size, mask=0.):
         X_ = X[i:i + batch_size]
         X_len = max([ len(x) for x in X_ ])
         X_padding = [ X_len - len(x) for x in X_ ]
+
         X_padded = [ x + [mask] * mask_len for x, mask_len  in zip(X_, X_padding) ]
         X_mask = [ [1]*len(x)  + [0]*mask_len for x, mask_len  in zip(X_, X_padding) ]
         ex.append(X_padded)
         masks.append(X_mask)
     return ex, masks
 
-class Seq2SeqAttention:
-    def __init__(self, collection, vocab_size, out_vocab_size, embedding_dim=128, encoder_layers=3, decoder_layers=3, \
-            encoder_hidden_dim=256, decoder_hidden_dim=256, encoder_dropout=0.3, decoder_dropout=0.3, attention_dropout=0.3):
-        self.collection = collection
-        self.params = {}
-
-        self.params['W_emb'] = collection.add_lookup_parameters((vocab_size, embedding_dim))
-        self.encoder = [ dy.LSTMBuilder(encoder_layers, embedding_dim, encoder_hidden_dim, collection), \
-                dy.LSTMBuilder(encoder_layers, embedding_dim, encoder_hidden_dim, collection) ]
-
-        self.decoder = [ dy.LSTMBuilder(1, encoder_hidden_dim, decoder_hidden_dim, collection), \
-                dy.LSTMBuilder(decoder_layers-1, decoder_hidden_dim, decoder_hidden_dim, collection) ]
-        self.params['W_1'] = collection.add_parameters((decoder_hidden_dim, encoder_hidden_dim)) 
-        self.params['W_2'] = collection.add_parameters((decoder_hidden_dim, decoder_hidden_dim)) 
-        self.params['vT'] = collection.add_parameters((1, decoder_hidden_dim,)) 
-
-        self.params['R'] = collection.add_parameters((out_vocab_size, decoder_hidden_dim)) 
-        self.params['b'] = collection.add_parameters((out_vocab_size,)) 
-
-        self.encoder_dropout = encoder_dropout
-        self.decoder_dropout = decoder_dropout
-        self.attention_dropout = attention_dropout
-
-    def one_sequence_batch(self, X_batch, X_reverse, maxlen, X_masks, training=True):
-        #params
-        W_emb = self.params['W_emb']
-        W_1 = dy.parameter(self.params['W_1'])
-        W_2 = dy.parameter(self.params['W_2'])
-        vT = dy.parameter(self.params['vT'])
-        R = dy.parameter(self.params['R'])
-        b = dy.parameter(self.params['b'])
-
-        if training:
-            self.encoder[0].set_dropouts(self.encoder_dropout, 0)
-            self.encoder[1].set_dropouts(self.encoder_dropout, 0)
-            self.decoder[0].set_dropouts(0, 0)
-            self.decoder[1].set_dropouts(self.decoder_dropout, 0)
-        else:
-            self.encoder[0].set_dropouts(0, 0)
-            self.encoder[1].set_dropouts(0, 0)
-            self.decoder[0].set_dropouts(0, 0)
-            self.decoder[1].set_dropouts(0, 0)
-
-        #encode
-        X_ = [ dy.lookup_batch(self.params['W_emb'], tok_batch) for tok_batch in X_reverse ]
-        X = [ dy.lookup_batch(self.params['W_emb'], tok_batch) for tok_batch in X_batch ]
-
-        lstm = self.encoder[0].initial_state()
-        states = lstm.add_inputs(X)
-        s1 = states[-1].s()
-        forward = [ state.h()[-1] for state in states ]
-
-        lstm = self.encoder[1].initial_state()
-        states = lstm.add_inputs(X_)
-        s2 = states[-1].s()
-        backward = [ state.h()[-1] for state in states ]
-
-        hidden_state = [ x + y for x, y in zip(s1, s2) ]        #hidden state concatenation
-        encoding = [ x + y for x, y in zip(forward, backward) ]
-
-        #decode
-        xs = [ W_1 * h_i for h_i in encoding ]
-        if training:
-            xs = [ dy.dropout(x, self.attention_dropout) for x in xs ]
-
-        encoding = dy.concatenate_cols(encoding)
-        c_0, h_0 = hidden_state[0], hidden_state[3]     #dependent on layers
-        s0 = self.decoder[0].initial_state(vecs=[c_0, h_0])
-        
-        hidden = []
-        state = s0
-        for tok in range(0, maxlen):
-            y = W_2 * state.h()[-1]
-            if training:
-                y = dy.dropout(y, self.attention_dropout)
-
-            u = vT * dy.tanh(dy.concatenate_cols([ x + y for x in xs ]))
-            a_t = dy.softmax(u)
-            d_t = encoding * dy.transpose(a_t)
-            
-            state = state.add_input(d_t)
-            hidden.append(state.h()[-1])
-
-        s0 = self.decoder[1].initial_state(vecs=hidden_state[1:3]+hidden_state[4:])
-        hidden = s0.transduce(hidden)
-
-        #logits
-        decoding = [ dy.affine_transform([b, R, h_i]) for h_i in hidden ]
-        return decoding
-
-    #takes logits
-    def to_sequence_batch(self, decoding, out_vocab):
-        batch_size = decoding[0].dim()[1]
-        decoding = [ dy.softmax(x) for x in decoding ]
-        decoding = [ dy.reshape(x, (len(out_vocab), batch_size), batch_size=1) for x in decoding ]
-        decoding = [ np.argmax(x.value(), axis=0) for x in decoding ]
-        decoding = [  [ x[i] for x in decoding ] for i in range(0, batch_size) ]
-        return [ [ out_vocab[y] for y in x ] for x in decoding ]
-
-    def one_batch(self, X_batch, y_batch, X_masks, y_masks, eos=9999, training=True):
-        eoses = [ X.index(eos) for X in X_batch ]
-        X_rev = [ x[eos-1::-1] + x[eos:] for x, eos in zip(X_batch, eoses) ]
-
-        batch_size = len(X_batch)
-        X_batch = zip(*X_batch)
-        X_rev = zip(*X_rev)
-        y_batch = zip(*y_batch)
-        y_masks = zip(*y_masks)
-
-        decoding = seq2seq.one_sequence_batch(X_batch, X_rev, len(y_batch), X_masks, training=training)
-        
-        batch_loss = []
-        for x, y, mask in zip(decoding, y_batch, y_masks):
-            mask_expr = dy.inputVector(mask)
-            mask = dy.reshape(mask_expr, (1,), batch_size)
-            batch_loss.append(mask * dy.pickneglogsoftmax_batch(x, y))
-        batch_loss = dy.esum(batch_loss)
-        batch_loss = dy.sum_batches(batch_loss)
-
-        return batch_loss, decoding
-        
 if __name__ == '__main__':
     print('Reading vocab...')
     in_vocab = read_vocab()
     in_vocab +=  ['<unk>', '<EOS>', '<mask>']
-    out_vocab = ['(', ')', '<TOK>', '<EOS>']
+    out_vocab = read_vocab(vocab='out_vocab')
+    out_vocab += ['<EOS>', '<mask>']
     print('Done.')
 
     print('Reading train/valid data...')
-    BATCH_SIZE = 128
+    BATCH_SIZE = 1
     _, X_train = ptb(section='wsj_2-21', directory='data/', column=0)
     _, y_train = ptb(section='wsj_2-21', directory='data/', column=1)
     X_train, y_train = sort_by_len(X_train, y_train)
@@ -218,14 +102,14 @@ if __name__ == '__main__':
     seq2seq = Seq2SeqAttention(collection, len(in_vocab), len(out_vocab))
     print('Done.')
 
-    print('Loading model...')
-    collection.populate(checkpoint)
-    print('Done.')
+    #print('Loading model...')
+    #collection.populate(checkpoint)
+    #print('Done.')
 
     print('Training model...')
     EPOCHS = 3000
-    trainer = dy.AdamTrainer(collection)
-    trainer.set_clip_threshold(50.0)
+    trainer = dy.AdamTrainer(collection, alpha=0.02, beta_1=0.85, beta_2=0.997, eps=1e-6)
+    trainer.set_clip_threshold(0.0)
 
     for epoch in range(1, EPOCHS+1):
         loss = 0.
@@ -260,7 +144,7 @@ if __name__ == '__main__':
         for i, (X_batch, y_batch, X_masks, y_masks, X_batch_raw, y_batch_raw) in \
                 enumerate(zip(X_valid_seq, y_valid_seq, X_valid_masks, y_valid_masks, X_valid_raw, y_valid_raw), 1):
             dy.renew_cg()
-            batch_loss, decoding = seq2seq.one_batch(X_batch, y_batch, X_masks, y_masks, training=False)
+            batch_loss, decoding = seq2seq.one_batch(X_batch, y_batch, X_masks, y_masks, training=True)
             loss += batch_loss.value()
 
             y_pred = seq2seq.to_sequence_batch(decoding, out_vocab)
--- model.py
+++ model.py
@@ -1,5 +1,5 @@
-import dynet as dy
 import numpy as np
+import _gdynet as dy
 
 class Seq2SeqAttention:
     def __init__(self, collection, vocab_size, out_vocab_size, embedding_dim=128, encoder_layers=3, decoder_layers=3, \
@@ -8,11 +8,11 @@ class Seq2SeqAttention:
         self.params = {}
 
         self.params['W_emb'] = collection.add_lookup_parameters((vocab_size, embedding_dim))
-        self.encoder = [ dy.LSTMBuilder(encoder_layers, embedding_dim, encoder_hidden_dim, collection), \
-                dy.LSTMBuilder(encoder_layers, embedding_dim, encoder_hidden_dim, collection) ]
+        self.params['Wout_emb'] = collection.add_lookup_parameters((out_vocab_size, embedding_dim))
+        self.encoder = dy.VanillaLSTMBuilder(encoder_layers, embedding_dim, encoder_hidden_dim, collection)
 
-        self.decoder = [ dy.LSTMBuilder(1, encoder_hidden_dim, decoder_hidden_dim, collection), \
-                dy.LSTMBuilder(decoder_layers-1, decoder_hidden_dim, decoder_hidden_dim, collection) ]
+        self.decoder = [ dy.VanillaLSTMBuilder(decoder_layers-1, embedding_dim, decoder_hidden_dim, collection), \
+                dy.VanillaLSTMBuilder(1, encoder_hidden_dim+decoder_hidden_dim, decoder_hidden_dim, collection) ]
         self.params['W_1'] = collection.add_parameters((decoder_hidden_dim, encoder_hidden_dim)) 
         self.params['W_2'] = collection.add_parameters((decoder_hidden_dim, decoder_hidden_dim)) 
         self.params['vT'] = collection.add_parameters((1, decoder_hidden_dim,)) 
@@ -24,9 +24,10 @@ class Seq2SeqAttention:
         self.decoder_dropout = decoder_dropout
         self.attention_dropout = attention_dropout
 
-    def one_sequence_batch(self, X_batch, X_reverse, maxlen, X_masks, training=True):
+    def one_sequence_batch(self, X_batch, maxlen, X_masks, eos=9999, teacher_forcing=None, training=True):
         #params
         W_emb = self.params['W_emb']
+        Wout_emb = self.params['Wout_emb']
         W_1 = dy.parameter(self.params['W_1'])
         W_2 = dy.parameter(self.params['W_2'])
         vT = dy.parameter(self.params['vT'])
@@ -34,84 +35,89 @@ class Seq2SeqAttention:
         b = dy.parameter(self.params['b'])
 
         if training:
-            self.encoder[0].set_dropouts(self.encoder_dropout, 0)
-            self.encoder[1].set_dropouts(self.encoder_dropout, 0)
+            self.encoder.set_dropouts(self.encoder_dropout, 0)
             self.decoder[0].set_dropouts(0, 0)
             self.decoder[1].set_dropouts(self.decoder_dropout, 0)
         else:
-            self.encoder[0].set_dropouts(0, 0)
-            self.encoder[1].set_dropouts(0, 0)
+            self.encoder.set_dropouts(0, 0)
             self.decoder[0].set_dropouts(0, 0)
             self.decoder[1].set_dropouts(0, 0)
 
         #encode
-        X_ = [ dy.lookup_batch(self.params['W_emb'], tok_batch) for tok_batch in X_reverse ]
-        X = [ dy.lookup_batch(self.params['W_emb'], tok_batch) for tok_batch in X_batch ]
-
-        lstm = self.encoder[0].initial_state()
+        X = [ dy.lookup_batch(W_emb, tok_batch) for tok_batch in X_batch ]
+        lstm = self.encoder.initial_state()
         states = lstm.add_inputs(X)
         s1 = states[-1].s()
         forward = [ state.h()[-1] for state in states ]
 
-        lstm = self.encoder[1].initial_state()
-        states = lstm.add_inputs(X_)
-        s2 = states[-1].s()
-        backward = [ state.h()[-1] for state in states ]
+        hidden_state = s1
+        encoding = forward
 
-        hidden_state = [ x + y for x, y in zip(s1, s2) ]        #hidden state concatenation
-        encoding = [ x + y for x, y in zip(forward, backward) ]
-
-        #decode
-        xs = [ W_1 * h_i for h_i in encoding ]
-        if training:
-            xs = [ dy.dropout(x, self.attention_dropout) for x in xs ]
+        for i, mask in enumerate(X_masks):
+            mask_expr = dy.inputVector(mask)
+            mask = dy.reshape(mask_expr, (1,), batch_size=len(X_masks[0]))
+            encoding[i] = dy.transpose(mask * dy.transpose(encoding[i]))
 
         encoding = dy.concatenate_cols(encoding)
-        c_0, h_0 = hidden_state[0], hidden_state[3]     #dependent on layers
-        s0 = self.decoder[0].initial_state(vecs=[c_0, h_0])
-        
-        hidden = []
-        state = s0
-        for tok in range(0, maxlen):
-            y = W_2 * state.h()[-1]
+
+        #decode
+        xs = W_1 * encoding
+        if training:
+            xs = dy.dropout(xs, self.attention_dropout)
+
+        s0_0 = self.decoder[0].initial_state(vecs=hidden_state[:2]+hidden_state[3:5])
+        s0_1 = self.decoder[1].initial_state(vecs=hidden_state[2:3]+hidden_state[5:])
+
+        decoding = []
+        state_0 = s0_0.add_input(dy.lookup_batch(self.params['W_emb'], [eos for i in range(0, len(X_batch[0]))]))
+        state_1 = s0_1
+
+        for i in range(0, maxlen):
+            #attention
+            y = W_2 * state_1.h()[-1]
             if training:
                 y = dy.dropout(y, self.attention_dropout)
 
-            u = vT * dy.tanh(dy.concatenate_cols([ x + y for x in xs ]))
+            u = vT * dy.tanh(dy.colwise_add(xs, y))
             a_t = dy.softmax(u)
             d_t = encoding * dy.transpose(a_t)
+
+            #input
+            history = state_0.h()[-1]
+            inp = dy.concatenate([history, d_t])
             
-            state = state.add_input(d_t)
-            hidden.append(state.h()[-1])
+            #next state
+            state_1 = state_1.add_input(inp)
+            h_i = state_1.h()[-1]
+            decoding.append(dy.softmax(dy.affine_transform([b, R, h_i])))
 
-        s0 = self.decoder[1].initial_state(vecs=hidden_state[1:3]+hidden_state[4:])
-        hidden = s0.transduce(hidden)
-
+            if training:
+                tf = dy.lookup_batch(Wout_emb, teacher_forcing[i])
+                state_0.add_input(tf)
+            else:
+                print('Not implemented yet!')
+        
         #logits
-        decoding = [ dy.affine_transform([b, R, h_i]) for h_i in hidden ]
         return decoding
 
     #takes logits
     def to_sequence_batch(self, decoding, out_vocab):
         batch_size = decoding[0].dim()[1]
-        decoding = [ dy.softmax(x) for x in decoding ]
         decoding = [ dy.reshape(x, (len(out_vocab), batch_size), batch_size=1) for x in decoding ]
         decoding = [ np.argmax(x.value(), axis=0) for x in decoding ]
         decoding = [  [ x[i] for x in decoding ] for i in range(0, batch_size) ]
         return [ [ out_vocab[y] for y in x ] for x in decoding ]
 
     def one_batch(self, X_batch, y_batch, X_masks, y_masks, eos=9999, training=True):
-        eoses = [ X.index(eos) for X in X_batch ]
-        X_rev = [ x[eos-1::-1] + x[eos:] for x, eos in zip(X_batch, eoses) ]
-
         batch_size = len(X_batch)
         X_batch = zip(*X_batch)
-        X_rev = zip(*X_rev)
+        X_masks = zip(*X_masks)
         y_batch = zip(*y_batch)
         y_masks = zip(*y_masks)
 
-        decoding = self.one_sequence_batch(X_batch, X_rev, len(y_batch), X_masks, training=training)
-        
+        decoding = self.one_sequence_batch(X_batch, len(y_batch), X_masks, \
+                eos=eos, teacher_forcing=y_batch, training=training)
+
         batch_loss = []
         for x, y, mask in zip(decoding, y_batch, y_masks):
             mask_expr = dy.inputVector(mask)
